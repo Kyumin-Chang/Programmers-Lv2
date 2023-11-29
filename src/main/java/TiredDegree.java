1752bdbf3709c242d11a79b29a2331fa7827639b
Kyumin-Chang <kyumin0529@naver.com>
2023-11-30 01:26:39 +0900

    feat Tired Degree

---
+++ src/main/java/TiredDegree.java
@@ -0,0 +1,36 @@
+public class TiredDegree {
+    /*
+    80	[[80,20],[50,40],[30,10]]	3
+     */
+    static int max = Integer.MIN_VALUE;
+
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(k, dungeons, visited);
+        return max;
+    }
+
+    public void dfs(int k, int[][] dungeons, boolean[] visited) {
+        int count = 0;
+        for (int i = 0; i < visited.length; i++) {
+            if (visited[i]) {
+                count++;
+            }
+        }
+        if (count > max) {
+            max = count;
+        }
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i]) {
+                if (dungeons[i][0] <= k) {
+                    visited[i] = true;
+                    k -= dungeons[i][1];
+                    dfs(k, dungeons, visited);
+                    k += dungeons[i][1];
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
