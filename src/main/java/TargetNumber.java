bd5762efed073d1e70f679faf4de9f0d5a5ae718
Kyumin-Chang <kyumin0529@naver.com>
2023-12-03 15:43:32 +0900

    refactor: Target Number

--- src/main/java/TargetNumber.java
+++ src/main/java/TargetNumber.java
@@ -1,6 +1,22 @@
 public class TargetNumber {
+    static int answer = 0;
+
     public int solution(int[] numbers, int target) {
-        int answer = 0;
+        dfs(0, 0, numbers, target);
         return answer;
+    }
+
+    public void dfs(int sum, int idx, int[] numbers, int target) {
+        if (sum == target && idx == numbers.length) {
+            answer++;
+            return;
+        }
+
+        if (idx >= numbers.length) {
+            return;
+        }
+
+        dfs(sum + numbers[idx], idx + 1, numbers, target);
+        dfs(sum - numbers[idx], idx + 1, numbers, target);
     }
 }
