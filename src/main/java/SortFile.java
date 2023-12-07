460a655c8b8133f109913d283b950b1b492f53e0
Kyumin-Chang <kyumin0529@naver.com>
2023-12-08 01:27:24 +0900

    feat : Sort File

---
+++ src/main/java/SortFile.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SortFile {
+    public String[] solution(String[] files) {
+        List<String> answerList = new ArrayList<>();
+        Arrays.sort(files);
+        for (int i = 0; i < files.length; i++) {
+            
+        }
+    }
+}
