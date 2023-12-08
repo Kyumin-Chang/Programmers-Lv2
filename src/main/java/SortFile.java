ec5a9afe56827c1b5352fcb53f89938cc7be7a69
Kyumin-Chang <kyumin0529@naver.com>
2023-12-08 23:31:28 +0900

    refactor: Sort File

--- src/main/java/SortFile.java
+++ src/main/java/SortFile.java
@@ -1,13 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class SortFile {
     public String[] solution(String[] files) {
-        List<String> answerList = new ArrayList<>();
-        Arrays.sort(files);
-        for (int i = 0; i < files.length; i++) {
-            
-        }
+
     }
 }
